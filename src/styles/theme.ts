c4bec4157031050559648e631d980f757e47ef57
se0jinYoon <tjwls1999@gmail.com>
2024-05-13 04:13:00 +0900

    init: theme 설정

---
+++ src/styles/theme.ts
@@ -0,0 +1,186 @@
+import { DefaultTheme } from 'styled-components';
+
+const colors = {
+  white: '#FFFFFF',
+  black: '#222222',
+  pink400: '#FF385C',
+  pink200: '#FED5DF',
+  gray100: '#F7F7F7',
+  gray200: '#F2F2F2',
+  gray400: '#E7E7E7',
+  gray600: '#C5C5C5',
+  gray700: '#999999',
+  gray800: '#6A6A6A',
+  yellow200: '#F0EFE9',
+  blue400: '#0053D5',
+};
+
+export type ColorTypes = typeof colors;
+
+interface Font {
+  family: string;
+  size: number;
+  weight: number;
+  height: string;
+  letterSpacing: number;
+}
+
+const FONT = ({ family, weight, size, height, letterSpacing }: Font): string => {
+  return `
+        font-family:${family};
+        font-size:${size}rem;
+        font-weight:${weight};
+        line-height:${height};
+        letter-spacing: ${letterSpacing}%;
+    `;
+};
+
+const fonts = {
+  heading01: FONT({
+    family: 'Pretendard',
+    size: 2.4,
+    weight: 600,
+    height: 'noraml',
+    letterSpacing: 0,
+  }),
+  heading02: FONT({
+    family: 'Pretendard',
+    size: 2.4,
+    weight: 500,
+    height: 'noraml',
+    letterSpacing: 0,
+  }),
+  heading03: FONT({
+    family: 'Pretendard',
+    size: 2,
+    weight: 600,
+    height: 'noraml',
+    letterSpacing: 0,
+  }),
+  heading04: FONT({
+    family: 'Pretendard',
+    size: 1.8,
+    weight: 600,
+    height: 'noraml',
+    letterSpacing: -3,
+  }),
+  title01: FONT({
+    family: 'Pretendard',
+    size: 1.6,
+    weight: 600,
+    height: '140',
+    letterSpacing: 0,
+  }),
+  title02_heavy: FONT({
+    family: 'Pretendard',
+    size: 1.4,
+    weight: 600,
+    height: '140',
+    letterSpacing: 0,
+  }),
+  title02_middle: FONT({
+    family: 'Pretendard',
+    size: 1.4,
+    weight: 500,
+    height: '130',
+    letterSpacing: 0,
+  }),
+  body01_heavy: FONT({
+    family: 'Pretendard',
+    size: 1.3,
+    weight: 600,
+    height: '135',
+    letterSpacing: -1,
+  }),
+  body01_middle: FONT({
+    family: 'Pretendard',
+    size: 1.3,
+    weight: 500,
+    height: '135',
+    letterSpacing: -1,
+  }),
+  body01_light: FONT({
+    family: 'Pretendard',
+    size: 1.3,
+    weight: 400,
+    height: '135',
+    letterSpacing: -1,
+  }),
+  body02_heavy: FONT({
+    family: 'Pretendard',
+    size: 1.2,
+    weight: 600,
+    height: '140',
+    letterSpacing: 0,
+  }),
+  body02_middle: FONT({
+    family: 'Pretendard',
+    size: 1.2,
+    weight: 500,
+    height: '160',
+    letterSpacing: 2,
+  }),
+  body03_heavy: FONT({
+    family: 'Pretendard',
+    size: 1.1,
+    weight: 600,
+    height: '140',
+    letterSpacing: 0,
+  }),
+  body03_middle: FONT({
+    family: 'Pretendard',
+    size: 1.1,
+    weight: 500,
+    height: '140',
+    letterSpacing: 0,
+  }),
+  body03_light: FONT({
+    family: 'Pretendard',
+    size: 1.1,
+    weight: 400,
+    height: '140',
+    letterSpacing: 0,
+  }),
+  detail1_heavy: FONT({
+    family: 'Pretendard',
+    size: 1,
+    weight: 600,
+    height: '140',
+    letterSpacing: 0,
+  }),
+  detail1_middle: FONT({
+    family: 'Pretendard',
+    size: 1,
+    weight: 500,
+    height: '140',
+    letterSpacing: 0,
+  }),
+  detail1_light: FONT({
+    family: 'Pretendard',
+    size: 1,
+    weight: 400,
+    height: '140',
+    letterSpacing: 0,
+  }),
+  detail2: FONT({
+    family: 'Pretendard',
+    size: 0.9,
+    weight: 400,
+    height: '140',
+    letterSpacing: 0,
+  }),
+  detail3: FONT({
+    family: 'Pretendard',
+    size: 0.8,
+    weight: 500,
+    height: '140',
+    letterSpacing: 0,
+  }),
+};
+
+export type FontsTypes = typeof fonts;
+
+export const theme: DefaultTheme = {
+  colors,
+  fonts,
+};
