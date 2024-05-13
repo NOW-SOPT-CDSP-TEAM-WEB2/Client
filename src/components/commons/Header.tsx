e32bcbe1b36b896a38ee07c69dd184c8a386586d
se0jinYoon <tjwls1999@gmail.com>
2024-05-14 04:34:42 +0900

    feat: 로고만 있는 헤더 구현

--- src/components/commons/Header.tsx
+++ src/components/commons/Header.tsx
@@ -1,7 +1,32 @@
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
 import { LogoIcon } from '../../assets/svgs';
 
-const Header = () => {
-  return <LogoIcon />;
+// 로고만 있는 헤더
+export const DefaultHeader = () => {
+  const navigate = useNavigate();
+  return (
+    <DefaultHeaderWrapper>
+      <LogoIc onClick={() => navigate('/')} />
+    </DefaultHeaderWrapper>
+  );
 };
 
-export default Header;
+const DefaultHeaderWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  z-index: 2;
+
+  display: flex;
+  justify-content: flex-start;
+  width: 1366px;
+  height: 6rem;
+  padding: 1.7rem 0 1.7rem 2rem;
+
+  background-color: ${({ theme }) => theme.colors.white};
+`;
+
+const LogoIc = styled(LogoIcon)`
+  cursor: pointer;
+`;
--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
 
-import Header from '../../components/commons/Header';
+import { DefaultHeader } from '../../components/commons/Header';
 
 const Main = () => {
   return (
     <TestContainer>
-      <Header />
+      <DefaultHeader />
     </TestContainer>
   );
 };
@@ -13,13 +13,5 @@ const Main = () => {
 export default Main;
 
 const TestContainer = styled.div`
-  width: 100%;
-  height: 30rem;
-  padding: 5.9rem;
-`;
-
-const Test = styled.div`
-  height: 3rem;
-
-  background-color: ${({ theme }) => theme.colors.blue400};
+  width: 1366px;
 `;
