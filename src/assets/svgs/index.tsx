ead3065769010cedb2f2a3993602a2a82412e9f8
ivoryeee <psa109@sookmyung.ac.kr>
2024-05-18 01:42:47 +0900

    [feat] svg 파일 추가

--- src/assets/svgs/index.tsx
+++ src/assets/svgs/index.tsx
@@ -1,8 +1,19 @@
 /* eslint-disable import/no-unresolved */
 // 명명규칙은 xxxxIcon으로 통일
-export { default as BackArrowIcon } from './ic_arrow_left_black_24.svg?react';
+export { default as AmexIcon } from './ic_amex_normal_24.svg?react';
+export { default as ArrowDownIcon } from './ic_arrow_down.svg?react';
+export { default as ArrowLeftBlackICon } from './ic_arrow_left_black_24.svg?react';
+export { default as DiamondPinkIcon } from './ic_diamond_pink_24.svg?react';
 export { default as HamburgerBlackIcon } from './ic_hamburger_black_12.svg?react';
 export { default as InternetBlackIcon } from './ic_internet_black_12.svg?react';
+export { default as MasterCardIcon } from './ic_mastercard_normal_24.svg?react';
+export { default as MasterCardNormalIcon } from './ic_mastercard_normal_32.svg?react';
+export { default as MedalFillBlackIcon } from './ic_medal_fill_black_10.svg?react';
+export { default as NaverPayIcon } from './ic_naverpay_normal_24.svg?react';
+export { default as StarIcon } from './ic_star.svg?react';
+export { default as TimePink32 } from './ic_time_pink_32.svg?react';
+export { default as VisaIcon } from './ic_visa_normal_24.svg?react';
 export { default as LogoIcon } from './logo.svg?react';
+export { default as PostReservationHousingCard } from './postReservation_housingCard.svg?react';
 export { default as ProfileImgIcon } from './profileImg.svg?react';
-export { default as DiamondPinkIcon } from './ic_diamond_pink_24.svg?react';
+export { default as SendingMessagetoHost } from './SendingMessagetoHost.svg?react';
