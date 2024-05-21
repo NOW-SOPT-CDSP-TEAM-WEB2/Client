3fc87789a8bee9b80d11bf101824a58984999905
kimkyuhong <bobo3766@naver.com>
2024-05-21 22:46:50 +0900

    feat: home router path 설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import Main from './pages/main/Main';
+import HomePage from './pages/main/HomePage/HomePage';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/main" element={<Main />} />
+        <Route path="/home" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
