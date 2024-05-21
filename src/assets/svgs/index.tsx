30cc6ba899f464d803320380ef71863fe7a79bc1
kimkyuhong <bobo3766@naver.com>
2024-05-21 22:48:40 +0900

    feat: svgr 컴포넌트 관리

--- src/assets/svgs/index.tsx
+++ src/assets/svgs/index.tsx
@@ -1,6 +1,21 @@
 /* eslint-disable import/no-unresolved */
 // 명명규칙은 xxxxIcon으로 통일
+export { default as CarouselLeftIcon } from './ic_arrow_left_circle_32.svg?react';
+export { default as CarouselrightIcon } from './ic_arrow_right_circle_32.svg?react';
+export { default as BeachBlackIcon } from './ic_beach_black_18.svg?react';
+export { default as BeachGrayIcon } from './ic_beach_gray_18.svg?react';
+export { default as DesignBlackIcon } from './ic_design_black_18.svg?react';
+export { default as DesignGrayIcon } from './ic_design_gray_18.svg?react';
 export { default as HamburgerBlackIcon } from './ic_hamburger_black_12.svg?react';
+export { default as HotBlackIcon } from './ic_hot_black_18.svg?react';
+export { default as HotGrayIcon } from './ic_hot_grey_18.svg?react';
 export { default as InternetBlackIcon } from './ic_internet_black_12.svg?react';
+export { default as ParkBlackIcon } from './ic_park_black_18.svg?react';
+export { default as ParkGrayIcon } from './ic_park_gray_18.svg?react';
+export { default as searchMiniWhiteIcon } from './ic_search_mini_white_12.svg?react';
+export { default as SwimmingPoolBlackIcon } from './ic_swimming_pool_black_18.svg?react';
+export { default as SwimmingPoolGrayIcon } from './ic_swimming_pool_gray_18.svg?react';
+export { default as UfoBlackIcon } from './ic_ufo_black_18.svg?react';
+export { default as UfoGrayIcon } from './ic_ufo_gray_18.svg?react';
 export { default as LogoIcon } from './logo.svg?react';
 export { default as ProfileImgIcon } from './profileImg.svg?react';
