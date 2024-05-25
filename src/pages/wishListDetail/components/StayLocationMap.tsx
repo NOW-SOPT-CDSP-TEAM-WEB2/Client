9879dc4686f743113a03464f2b1a40b040d98aa0
se0jinYoon <tjwls1999@gmail.com>
2024-05-25 10:53:49 +0900

    fix: 위시 상세 지도 오류 수정

--- src/pages/wishListDetail/components/StayLocationMap.tsx
+++ src/pages/wishListDetail/components/StayLocationMap.tsx
@@ -1,38 +1,39 @@
 /* eslint-disable simple-import-sort/imports */
+import { useState, useEffect } from 'react';
 import { Map, MapMarker, ZoomControl } from 'react-kakao-maps-sdk';
 import styled from 'styled-components';
 
+import { wishListApiDataType } from '../../wishList/utils/getWishList';
 import { latlngType } from '../WishListDetail';
 
 interface StayLocationMapProps {
-  latlngList: latlngType[];
+  wishList: wishListApiDataType[];
 }
 
 const StayLocationMap = (props: StayLocationMapProps) => {
-  const { latlngList } = props;
+  const { wishList } = props;
+  const [latLngList, setLatLngList] = useState<latlngType[]>([]);
 
-  if (latlngList.length === 0) {
-    return (
-      <StyledMap center={{ lat: 33.5563, lng: 126.79581 }} level={13}>
-        <MapMarker
-          position={{ lat: 33.55635, lng: 126.795841 }}
-          image={{
-            src: './src/assets/svgs/btn_ic_circle_pin_amall.svg',
-            size: {
-              width: 50,
-              height: 50,
-            },
-          }}
-        />
+  const convertLatLng = (roomData: wishListApiDataType[]) => {
+    return roomData.map((item) => ({
+      latlng: { lat: item.latitude, lng: item.longitude },
+    }));
+  };
 
-        <ZoomControl />
-      </StyledMap>
-    );
+  useEffect(() => {
+    if (wishList.length !== 0) {
+      const convertedWishLocList = convertLatLng(wishList);
+      setLatLngList(convertedWishLocList);
+    }
+  }, [wishList]);
+
+  if (latLngList.length === 0) {
+    return <></>;
   }
 
   return (
-    <StyledMap center={latlngList[0].latlng} level={13}>
-      {latlngList.map((position, idx) => (
+    <StyledMap center={latLngList[0].latlng} level={13}>
+      {latLngList.map((position, idx) => (
         <MapMarker
           key={`${position.latlng}-${idx}`}
           position={position.latlng}
